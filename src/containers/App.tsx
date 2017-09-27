8a164223fc718984f96ac97de53ec2df223696b0
Jacob Cole <tmad4000@gmail.com>
2017-09-27 16:05:08 -0700

    init

---
+++ src/containers/App.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+
+export interface AppState {
+}
+
+
+export interface AppProps extends React.Props<App> {
+}
+
+
+export default class App extends React.Component<AppProps, AppState> {
+
+
+
+    render() {
+
+
+        return (<div>
+
+
+
+hi
+
+        </div>)
+
+
+    }
+}
